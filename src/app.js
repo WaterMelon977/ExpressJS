3806cd9719207aa6ab39054589248ac8b6a83b0e
WaterMelon977 <149487960+WaterMelon977@users.noreply.github.com>
2024-01-24 09:53:43 +0000

    mustache incorporated

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const path = require('path');
+
+const JOBS = require('./jobs');
+const mustacheExpress = require('mustache-express');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+// Configure mustache
+app.set('views', `${__dirname}/pages`);
+app.set('view engine', 'mustache');
+app.engine('mustache', mustacheExpress());
+
 
 app.get('/', (req, res) => {
     // res.send('Hello sumanth wassup, my man');
-    res.sendFile(path.join(__dirname, 'pages/index.html'));
+    // res.sendFile(path.join(__dirname, 'pages/index.html'));
+    res.render('index',{jobs: JOBS});
 
 });
 
